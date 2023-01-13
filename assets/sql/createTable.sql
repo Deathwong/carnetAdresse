4252b97c18202c18cb67aecba78dee9bbf7c5473
Deathwong <baptistejean457@gmail.com>
2023-01-13 01:06:54 +0100

    création des classes Contact et Adresse

--- assets/sql/createTable.sql
+++ assets/sql/createTable.sql
@@ -1,3 +1,19 @@
+create table IF NOT EXISTS adresse (
+    id INT NOT NULL AUTO_INCREMENT,
+    numeroRue INT (3),
+    nomRue VARCHAR (50),
+    ville VARCHAR (50) not NULL,
+    pays VARCHAR (20) not null,
+    codePostal VARCHAR (5) not null,
+    region VARCHAR (100),
+    departement VARCHAR (100),
+    lieuDit VARCHAR (10),
+    complementUn VARCHAR (50),
+    complementDeux VARCHAR (50),
+    complementTrois VARCHAR (50),
+    PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
 create table IF NOT EXISTS contact (
     id INT NOT NULL AUTO_INCREMENT,
     idAdresse INT,
@@ -14,22 +30,5 @@ create table IF NOT EXISTS contact (
    	situationFamiliale VARCHAR(100),
     note VARCHAR (100),
     PRIMARY KEY (id),
-    FOREIGN key (idAdresse) references adresse(id)
-);
-
-
-create table IF NOT EXISTS adresse (
-    id INT NOT NULL AUTO_INCREMENT,
-    numeroRue INT (3),
-    nomRue VARCHAR (50),
-    ville VARCHAR (50) not NULL,
-    pays VARCHAR (20) not null,
-    codePostal VARCHAR (5) not null,
-    region VARCHAR (100),
-    departement VARCHAR (100),
-    lieuDit VARCHAR (10),
-    complementUn VARCHAR (50),
-    complementDeux VARCHAR (50),
-    complementTrois VARCHAR (50),
-    PRIMARY KEY (id)
-);
+    constraint fk_adresse FOREIGN key (idAdresse) references adresse(id)
+)ENGINE=InnoDB;
